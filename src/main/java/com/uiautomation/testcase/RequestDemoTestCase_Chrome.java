c2a3cff8c55682bba607336ffe5214b363538f1a
mesuj <mesuj@DESKTOP-BAQS1II>
2023-02-12 18:54:08 +0530

    Code added to execute script in remote server.

--- src/main/java/com/uiautomation/testcase/RequestDemoTestCase_Chrome.java
+++ src/main/java/com/uiautomation/testcase/RequestDemoTestCase_Chrome.java
@@ -1,5 +1,11 @@
 package com.uiautomation.testcase;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
@@ -11,12 +17,12 @@ import com.uiautomation.utilities.BrowserFactory;
 
 
 
-public class RequestDemoTestCase {
+public class RequestDemoTestCase_Chrome {
 	public WebDriver driver;
 	
 	
 	@BeforeMethod
-	public void launchbrowser()
+	public void launchbrowser() throws MalformedURLException
 	{
 		driver=BrowserFactory.startBrowser("Chrome", "https://freshdesk.com/");
 	}
@@ -24,6 +30,7 @@ public class RequestDemoTestCase {
 	@Test
 	public void verifyRequestDemo() throws InterruptedException
 	{
+		System.out.println("Tests running from Chrome Browser");
 		System.out.println("Title of the Page :" +driver.getTitle());
 		HomePage homepage=PageFactory.initElements(driver, HomePage.class);
 		homepage.click();
--- src/main/java/com/uiautomation/utilities/BrowserFactory.java
+++ src/main/java/com/uiautomation/utilities/BrowserFactory.java
@@ -1,37 +1,58 @@
 package com.uiautomation.utilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
-
-public class BrowserFactory
-{
-	private BrowserFactory()
-	{
+public class BrowserFactory {
+	private BrowserFactory() {
 		throw new IllegalStateException("Utility class");
 	}
+
 	static WebDriver driver;
-	
-	public static WebDriver startBrowser(String browsername,String url)
-	{
-		if(browsername.equalsIgnoreCase("FireFox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
-		}else if(browsername.equalsIgnoreCase("Chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
+//	@SuppressWarnings("rawtypes")
+//	static AbstractDriverOptions options = null;
+//	static ChromeOptions chromeOptions=null;
+
+	public static WebDriver startBrowser(String browsername, String url) throws MalformedURLException {
+		if (browsername.equalsIgnoreCase("FireFox")) {
+
+			driver = new FirefoxDriver();
+//====================Below options to run testcase in docker=======
+//			options = new FirefoxOptions();
+//			options.setCapability("browsername", "firefox");
+
+		} else if (browsername.equalsIgnoreCase("Chrome")) {
+
+//====================Below options to run testcase in docker=======
+//			options = new ChromeOptions();
+//			options.setCapability("browsername", "chrome");
+//====================Below options for Zalenium=====================
+//			chromeOptions= new ChromeOptions();
+//			chromeOptions.addArguments("--disable-dev-shm-usage");
+			driver = new ChromeDriver();
+
+		} else if (browsername.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+//====================Below options to run testcase in docker=======
+//			options = new EdgeOptions();
+//			options.setCapability("browsername", "edge");
 		}
-		
+//================below options to run test case in remote server like docker==============
+//		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
 		driver.get(url);
 		return driver;
 	}
