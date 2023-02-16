f1529dfbbc1f7a77c124d198306c70b82e462cc2
mesuj <mesuj@DESKTOP-BAQS1II>
2023-02-16 17:54:08 +0530

    Updated testng.groovy

---
+++ testng.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Get Source Code') {
+            steps {
+                git credentialsId: 'Git-Credentials', url: 'https://github.com/rakesh0277/demoproject.git'
+                echo 'Hello World'
+            }
+        }
+        stage('Build Code')
+        {
+            steps
+            {
+                bat script'mvn compile'
+            }
+        }
+        stage('Run Test')
+        {
+            steps
+            {
+                bat script 'mvn test -Dbrowser=localchrome'
+            }
+        }
+        
+    }
+}
